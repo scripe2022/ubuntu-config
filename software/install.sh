6a85128f8d5a6b1a788aa27c09457e8e06325f99
Yuhang <im@jyh.sb>
2024-04-01 16:05:26 -0700

    last

---
+++ software/install.sh
@@ -0,0 +1,16 @@
+# slack
+flatpak install flathub com.slack.Slack
+# todoist
+flatpak install flathub com.todoist.Todoist
+# discord
+flatpak install flathub com.discordapp.Discord
+# postman
+flatpak install flathub com.getpostman.Postman
+# spotify
+flatpak install flathub com.spotify.Client
+# zoom
+flatpak install flathub us.zoom.Zoom
+# onlyoffice
+flatpak install flathub org.onlyoffice.desktopeditors
+# upnote
+snap install upnote
