a20285de09cd2d65db57ec40b9f2a4331a872831
Yuhang <im@jyh.sb>
2024-04-01 15:54:28 -0700

    bauh

--- install.sh
+++ install.sh
@@ -16,6 +16,8 @@ source $HOME/.bashrc
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y build-essential cmake curl git gnome-tweaks fd-find ripgrep wl-clipboard tree-sitter-cli linux-headers-$(uname -r) gettext libfuse2 gnome-browser-connector tree libnvidia-egl-wayland1 gir1.2-gtk-4.0 python3-nautilus gir1.2-gda-5.0 gir1.2-gsound-1.0 xclip nfs-kernel-server
+# bauh dependencies
+sudo apt install -y qt5dxcb-plugin libcanberra-gtk-module aptitude timeshift aria2 axel sqlite3 flatpak snapd python3-lxml python3-bs4 python3-venv xdg-utils fuse3
 sudo apt install -y ibus-pinyin vlc webp flameshot solaar
 sudo apt remove nautilus-extension-gnome-terminal
 
@@ -25,17 +27,9 @@ sudo apt remove nautilus-extension-gnome-terminal
 . ./cpplibs/install.sh
 . ./bin/install.sh
 . ./nvim/install.sh
-. ./nvidia/install.sh
 . ./1password/install.sh
 . ./utils/install.sh
-. ./spotify/install.sh
 . ./sublime/install.sh
-. ./discord/install.sh
-. ./onlyoffice/install.sh
-. ./slack/install.sh
-. ./upnote/install.sh
-. ./todoist/install.sh
-. ./postman/install.sh
 . ./docker/install.sh
-
+. ./nvidia/install.sh
 
--- postman/install.sh
+++
@@ -1,1 +0,0 @@
-snap install postman
