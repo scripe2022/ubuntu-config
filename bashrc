c8f080de3a7e9ba9386f44d085e658872f8feb1d
Yuhang <im@jyh.sb>
2024-04-05 15:41:39 -0700

    xsel && merge

--- bashrc
+++ bashrc
@@ -1,4 +1,4 @@
 # cpp path
 export CPLUS_INCLUDE_PATH="$HOME/.local/include:${CPLUS_INCLUDE_PATH}"
 export PATH="$HOME/.local/bin:${PATH}"
-
+alias merge="/opt/sublime_merge/sublime_merge"
--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ source $HOME/.bashrc
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y build-essential cmake curl git gnome-tweaks fd-find ripgrep wl-clipboard tree-sitter-cli linux-headers-$(uname -r) gettext libfuse2 gnome-browser-connector tree libnvidia-egl-wayland1 gir1.2-gtk-4.0 python3-nautilus gir1.2-gda-5.0 gir1.2-gsound-1.0 xclip nfs-kernel-server
+sudo apt install -y build-essential cmake curl git gnome-tweaks fd-find ripgrep wl-clipboard tree-sitter-cli linux-headers-$(uname -r) gettext libfuse2 gnome-browser-connector tree libnvidia-egl-wayland1 gir1.2-gtk-4.0 python3-nautilus gir1.2-gda-5.0 gir1.2-gsound-1.0 xsel nfs-kernel-server
 # bauh dependencies
 sudo apt install -y qt5dxcb-plugin libcanberra-gtk-module aptitude timeshift aria2 axel sqlite3 flatpak snapd python3-lxml python3-bs4 python3-venv xdg-utils fuse3
 sudo apt install -y ibus-pinyin vlc webp flameshot solaar
